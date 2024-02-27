cd1d1c0423a528fa0eb0d83e68e6560ed190eb55
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-02-27 22:53:49 +0300

    """comment is added to function get_json_file"""

--- classes/functions.py
+++ classes/functions.py
@@ -2,6 +2,7 @@ import json, os
 
 
 def get_json_file():
+    """Get json list format"""
     path_for_file = os.path.join('source', 'operations.json')
     with open(path_for_file, mode='r', encoding='utf-8') as f:
         file = json.load(f)
