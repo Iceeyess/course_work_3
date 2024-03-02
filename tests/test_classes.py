a0eef12006aef88fa841bd7d19f56c513c350440
Dmitry Krasheninnikov <ice_eyes@mail.ru>
2024-03-02 11:34:47 +0300

    test_classees module is setup for pytest.

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,5 @@
+import pytest
 import json
-import unittest
 from classes.classes import Check
 from _datetime import datetime
 
@@ -21,27 +21,29 @@ json_sample = '''[{
 dict_json = json.loads(json_sample)
 
 
-class TestCalc(unittest.TestCase):
+@pytest.fixture
+def check_list():
+    return [Check(check_file.get('id'), check_file.get('state'), check_file.get('date'),
+                  check_file.get('operationAmount'), check_file.get('description'), check_file.get('from'),
+                  check_file.get('to')) for check_file in dict_json if check_file]
 
-    def setUp(self):
-        """Определяем инициализацию класса стандартным методом unittest setUp
-        через присвоение случайновыбранного словаря."""
-        checks_list = [Check(check_file.get('id'), check_file.get('state'), check_file.get('date'),
-                             check_file.get('operationAmount'), check_file.get('description'), check_file.get('from'),
-                             check_file.get('to')) for check_file in dict_json if check_file]
-        self.verification = checks_list[0]
 
-    def test_format_date_string(self):
-        self.assertEqual(self.verification.format_date_string('2019-12-08T22:46:21.935582'),
-                         datetime(2019, 12, 8, 22, 46, 21, 935582))
+@pytest.fixture
+def verification(check_list):
+    return check_list[0]
 
-    def test_str_date_format(self):
-        self.assertEqual(self.verification.str_date_format(datetime(2019, 12, 8, 22, 46, 21, 935582)), '08.12.2019')
 
-    def test_get_card_number_account_number(self):
-        self.assertEqual(self.verification.get_card_number_account_number('Счет 90424923579946435907'), 'Счет **5907')
-        self.assertEqual(self.verification.get_card_number_account_number(None), 'Нет данных. Открытие вклада')
-        self.assertEqual(self.verification.get_card_number_account_number('Visa Platinum 1246377376343588'), 'Visa Platinum 1246 37XX XXXX 3588')
+def test_format_date_string(verification):
+    assert verification.format_date_string('2019-12-08T22:46:21.935582') == datetime(2019, 12, 8, 22, 46, 21, 935582)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_str_date_format(verification):
+    assert verification.str_date_format(datetime(2019, 12, 8, 22, 46, 21, 935582)) == '08.12.2019'
+
+
+def test_get_card_number_account_number(verification):
+    assert verification.get_card_number_account_number('Счет 90424923579946435907') == 'Счет **5907'
+    assert verification.get_card_number_account_number(None) == 'Нет данных. Открытие вклада'
+    assert verification.get_card_number_account_number('Visa Platinum 1246377376343588') == 'Visa Platinum 1246 37XX XXXX 3588'
+
+
